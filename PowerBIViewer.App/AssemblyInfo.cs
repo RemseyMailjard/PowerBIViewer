4942132d5147429d3c3eff548b855545394f5d72
Remsey Mailjard <remsey@skills4-it.nl>
2025-06-08 03:22:55 +0200

    Update

--- PowerBIViewer.App/AssemblyInfo.cs
+++ PowerBIViewer.App/AssemblyInfo.cs
@@ -1,10 +1,23 @@
+// FILE: AssemblyInfo.cs
+
+using System.Runtime.InteropServices;
 using System.Windows;
 
+// De meeste assembly-informatie wordt nu beheerd via het .csproj-bestand.
+// We laten hier alleen de attributen staan die specifiek zijn voor het project
+// en niet eenvoudig via de project-properties kunnen worden ingesteld.
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+// Dit is een unieke identifier voor je project. Je kunt deze zo laten.
+[assembly: Guid("CE5D849A-7AB9-4CDC-B6EB-A0AC467EB42C")]
+
+// ThemeInfo-attribuut voor WPF. Dit vertelt de framework hoe om te gaan met theming.
 [assembly: ThemeInfo(
-    ResourceDictionaryLocation.None,            //where theme specific resource dictionaries are located
-                                                //(used if a resource is not found in the page,
-                                                // or application resource dictionaries)
-    ResourceDictionaryLocation.SourceAssembly   //where the generic resource dictionary is located
-                                                //(used if a resource is not found in the page,
-                                                // app, or any theme specific resource dictionaries)
+    ResourceDictionaryLocation.None,            // Geen speciale stijlen voor Windows-thema's.
+    ResourceDictionaryLocation.SourceAssembly   // De generieke, standaard stijlen zitten in de assembly's zelf.
 )]
